dfc9dd614186b19d69035addf08716c77b142298
ErmisCat <ermis@skiomusic.com>
2018-03-07 13:38:31 -0800

    Added default lifecycle policy

--- replicat0r.sh
+++ replicat0r.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #---------------------------------------------------------------------------------------------------#
-# KMS Key Should be on the same Region as Destination
-# Ideally on a seperate account, with trust relationships giving access to the key to encrypt. (RO)
-#---------------------------------------------------------------------------------------------------#
 # Include Config Variables
 # Remember to edit the setup.conf file with your own environment details
 #-------------------#
@@ -22,39 +19,37 @@ VAULTS="aws-vault exec ${SPROFILE} --"
 echo "Creating buckets in destination..."
 for bucket in $(cat ./publish/s3_src.txt);
 do
-    $VAULTS aws s3api put-bucket-versioning --bucket ${bucket} --versioning-configuration Status=Enabled;
+    
     echo "Creating s3 bucket in destination: ${bucket}"
     $VAULTD aws s3 mb s3://${bucket}-replica --region ${DSTREGION};
-    $VAULTD aws s3api put-bucket-versioning --bucket ${bucket}-replica --versioning-configuration Status=Enabled;
-
-
-    echo "Applying bucket policy in destination..."
-
-    echo "Applying policy to bucket: ${bucket}"
-    sed "s/BUCKET/$bucket/g; s/XACCOUNTX/$SRCACCOUNTID/g;" ./conf/dst_policy.txt > ./publish/dst_policy.json;
     
+    echo "Enabling versioning on source & destination buckets... ${bucket}(-repica)"
+    $VAULTS aws s3api put-bucket-versioning --bucket ${bucket} --versioning-configuration Status=Enabled;
+    $VAULTD aws s3api put-bucket-versioning --bucket ${bucket}-replica --versioning-configuration Status=Enabled;
+    
+    echo "Enabling Lifecycle policy on buckets.. ${bucket}(-replica)"
+    $VAULTS aws s3api put-bucket-lifecycle-configuration --bucket ${bucket} --lifecycle-configuration file://publish/lifecycle.json;
+    $VAULTD aws s3api put-bucket-lifecycle-configuration --bucket ${bucket}-replica --lifecycle-configuration file://publish/lifecycle.json
+
+    echo "Applying policy to destination bucket: ${bucket}"
+    sed "s/BUCKET/$bucket/g; s/XACCOUNTX/$SRCACCOUNTID/g;" ./conf/dst_policy.txt > ./publish/dst_policy.json;
     $VAULTD aws s3api put-bucket-policy --bucket ${bucket}-replica --policy file://publish/dst_policy.json --region ${SRCREGION};
 
     echo "Creating Trust roles: "
-
     $VAULTS aws iam create-role --role-name crrRole-${bucket} --assume-role-policy-document file://publish/src_trust.json;
 
     echo "Creating policy(s) on source..."
-
     sed "s/BUCKET/$bucket/g; s/XSRCREGIONX/$SRCREGION/g; s/XDSTREGIONX/$DSTREGION/g;" ./conf/src_policy.txt > ./publish/src_policy.json;
-    
     ARN=$($VAULTS aws iam create-policy --policy-name crrPolicy-${bucket} --policy-document file://publish/src_policy.json --output text | cut -f2 -d$'\t');
-
     $VAULTS aws iam attach-role-policy --role-name crrRole-${bucket} --policy-arn ${ARN};
 
     echo "Setup s3 replication on source..."
-
     sed "s/BUCKET/$bucket/g; s/XACCOUNTX/$SRCACCOUNTID/g; s/XROLEX/crrRole-${bucket}/g; s/DACCOUNTD/$DSTACCOUNTID/g;" ./conf/src_replica.txt > ./publish/src_replica.json;
-
     $VAULTS aws s3api put-bucket-replication --bucket ${bucket} --replication-configuration file://publish/src_replica.json;
 
-    echo "Finished *-[${bucket}]-*"
-done
+    echo "Confirm CRR policy: ${bucket}..."
+    $VAULTS aws s3api get-bucket-replication --bucket ${bucket}
 
-# Confirm replication rule:
-#$VAULTS aws s3api get-bucket-replication --bucket ${bucket}
+    echo "Finished *-[${bucket}]-*"
+
+done
